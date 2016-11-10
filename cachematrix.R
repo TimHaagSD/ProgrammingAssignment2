7aec40ab5d5f31b57c1c4f31452b83e2577a97a0
TimHaagSD <TimHaagSD@gmail.com>
2016-11-09 20:46:10 -0800

    Assignment Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Create pratice matrix and verify value of inverse.
+x=rbind(c(1, 4), c(4, 1)) 
+x
+solve(x)
+## Function caches inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'x'
+## First looks to the cached data, then solves for the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m  
 }
